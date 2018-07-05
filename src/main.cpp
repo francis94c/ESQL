ea36bc8c2c9531dde5916a8f67d6e9fdac9a4a6d
Francis-PC\Francis <francis94c@gmail.com>
2018-07-05 19:06:09 +0100

    Initial Commit.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 5 July 2018
+ *      Author: Francis
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
+}
+
